ccb5e07ceac38ee912b5d346c655d7195931eeef
Omarzion <odama666@gmail.com>
2016-07-24 03:29:50 -0500

    got sound capture working

--- static/js/soundInput.js
+++ static/js/soundInput.js
@@ -1,28 +1,29 @@
 var audioContext;
 var recorder;
 var _debug = true;
-var soundInputInitialized = false;
 
 function soundInputInit() {
-	if (!soundInputInitialized) {
-	
-		try {
-			window.AudioContext = window.AudioContext || window.webkitAudioContext;
-			navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || navigator.webkitGetUserMedia;
-			window.URL = window.URL || window.webkitURL;
+		return new Promise(function(resolve, reject) {
+			try {
+				window.AudioContext = window.AudioContext || window.webkitAudioContext;
+				navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || navigator.webkitGetUserMedia;
+				window.URL = window.URL || window.webkitURL;
 		
-			audioContext = new AudioContext;
-			debugLog('Audio context set up.');
-			debugLog('navigator.mediaDevices.getUserMedia ' + (navigator.mediaDevices.getUserMedia ? 'available.' : 'not present!'));
-			soundInputInitialized = true;
-		} catch (e) {
-			alert('No web audio support in this browser!');
-		}
+				audioContext = new AudioContext;
+				debugLog('Audio context set up.');
+				debugLog('navigator.mediaDevices.getUserMedia ' + (navigator.mediaDevices.getUserMedia ? 'available.' : 'not present!'));
+			} catch (e) {
+				alert('No web audio support in this browser!');
+			}
 	
-		navigator.mediaDevices.getUserMedia({audio: true}, startMediaStream, function(e) {
-			debugLog('No live audio input: ' + e);
+			navigator.mediaDevices.getUserMedia({audio: true}).then(function(stream) {
+																												startMediaStream(stream);
+																												resolve(stream);
+																												}).catch(function(e) {
+				debugLog('No live audio input: ' + e);
+				reject(e);
+			});
 		});
-	}
 }
 
 function startMediaStream(stream) {
@@ -33,14 +34,19 @@ function startMediaStream(stream) {
 }
 
 function startRecording() {
-	recorder && recorder.record();
-	$('#mic-button').addClass('hidden');
-	$('#stop-button').removeClass('hidden');
+	soundInputInit().then(function(stream) {
+		recorder && recorder.record();
+		$('#mic-button').addClass('hidden');
+		$('#stop-button').removeClass('hidden');
+	});
 }
 
 function stopRecording() {
 	recorder && recorder.stop();
 	$('#stop-button').addClass('hidden');
+	
+	createDownloadLink();
+	//recorder.clear();
 	showPrompt();
 }
 
@@ -62,6 +68,24 @@ function showPrompt() {
 function hidePrompt() {
 	$('#accept-button').addClass('hidden');
 	$('#cancel-button').addClass('hidden');
+}
+
+function createDownloadLink() { // TODO remove this and post it to db
+	recorder && recorder.exportWAV(function(blob) {
+		var url = URL.createObjectURL(blob);
+		var li = document.createElement('li');
+		var au = document.createElement('audio');
+		var hf = document.createElement('a');
+		
+		au.controls = true;
+		au.src = url;
+		hf.href = url;
+		hf.download = new Date().toISOString() + '.wav';
+		hf.innerHTML = hf.download;
+		li.appendChild(au);
+		li.appendChild(hf);
+		recordingslist.appendChild(li);
+	});
 }
 
 function createInputWindow() {
